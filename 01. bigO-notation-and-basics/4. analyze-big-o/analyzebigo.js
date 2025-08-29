d64198007aeb2f10619b00e3bf01073bfa9b06ad
ujjalballen <ujjal.vision02@gmail.com>
2025-08-29 21:47:19 +0600

    analyze big o

---
+++ 01. bigO-notation-and-basics/4. analyze-big-o/analyzebigo.js
@@ -0,0 +1,12 @@
+const myObject = {
+    name: 'Ujjal B. Allen',
+    age: 24,
+    address: 'Dhaka',
+    phone: 1234567890
+}
+
+
+// const result = Object.keys(myObject); // time and space complexity O(n)
+// const result = Object.values(myObject); // time and space complexity O(n)
+// const result = myObject.hasOwnProperty('name') time and space complexity O(1)
+console.log(result)
