98808da4b4786b4f0daaae1b1610572c2b760702
ujjalballen <ujjal.vision02@gmail.com>
2025-09-07 17:43:19 +0600

    solve best way:find the second largest number in an array

--- 01. bigO-notation-and-basics/6. arrays/secondLargest.js
+++ 01. bigO-notation-and-basics/6. arrays/secondLargest.js
@@ -38,19 +38,19 @@ method: has(), delete(), add(), clear()
 const secondLargest = (array) => {
     // time complexity is O(n)
     // Space = O(n) (because we make a new Set and a new array) 
-    const unique = Array.from(new Set(array));  
-    console.log(unique)
+    const unique = Array.from(new Set(array));
+    // console.log(unique)
 
 
 
     // time complexity is O(log n)
-    unique.sort((a, b) => { 
+    unique.sort((a, b) => {
         return b - a;
     });
 
-    if(unique.length >=2 ){
+    if (unique.length >= 2) {
         return unique[1];
-    }else{
+    } else {
         return -1
     }
 
@@ -59,5 +59,49 @@ const secondLargest = (array) => {
 
 const num = [10];
 
-const result = secondLargest(num);
+// const result = secondLargest(num);
+// console.log(result)
+
+
+
+
+// Optimised Approach
+
+
+/* 
+# Time Complexity (O(n))
+
+   The loop runs n times, where n is the length of the array.
+   If the array size grows, the loop also grows and runs that many times.
+
+# Space Complexity (O(1))
+
+  We only keep a few variables (largest, secondLargest, currentNumber).
+  They don’t depend on the array size.
+  That’s why it uses constant space.
+*/
+
+const secondLargestOpt = (arr) => {
+    let largest = Number.NEGATIVE_INFINITY; // 55
+    let secondLargest = Number.NEGATIVE_INFINITY; // 20
+
+    for (let i = 0; i < arr.length; i++) {  // time complexity =  O(n); space complexity O(1)
+        const currentNumber = arr[i];
+
+        if (currentNumber > largest) { // comparisons = O(1) time complexity
+            secondLargest = largest;
+            largest = currentNumber;
+
+        } else if (currentNumber !== largest && currentNumber > secondLargest) {
+            secondLargest = currentNumber;
+        };
+    };
+
+    return secondLargest;
+
+}
+
+const num3 = [10, 5, 20, 8, 55];
+
+const result = secondLargest(num3);
 console.log(result)
